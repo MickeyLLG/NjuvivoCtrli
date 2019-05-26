cac048049c9ecb81992f57932476d78c88d7aeba
LRC <985088550@qq.com>
2019-05-26 16:57:08 +0800

    StudentClassDao

--- src/main/java/com/ctrli/mooc/dao/impl/AnalysisDaoImpl.java
+++ src/main/java/com/ctrli/mooc/dao/impl/AnalysisDaoImpl.java
@@ -5,6 +5,8 @@ import com.ctrli.mooc.dao.ClazzDao;
 import com.ctrli.mooc.entity.AnalysisEntity;
 import com.ctrli.mooc.entity.AnalysisEntityPK;
 import com.ctrli.mooc.entity.ClazzEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AnalysisDaoImpl extends BaseDaoImpl<AnalysisEntityPK, AnalysisEntity> implements AnalysisDao {
 }
